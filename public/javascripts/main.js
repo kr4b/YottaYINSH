2bf49c38e268748529d5ce1ca08dc22542591afa
Vincent Groen <33351005+The-Coding-Sloth@users.noreply.github.com>
2019-12-24 16:38:05 +0100

    Added methods for marker and ring movement

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,7 @@ onload = () => {
   const b = new Board(document.getElementById("yinsh-board"));
   b.render();
 
+
   update();
   function update() {
 
@@ -15,6 +16,14 @@ onload = () => {
     const canv = b.getCanvasCoordinate(yinsh.vertical, yinsh.point);
     b.ctx.strokeRect(canv.x - 3, canv.y - 3, 6, 6);
     b.ctx.strokeRect(mx - 3 - b.ctx.canvas.offsetLeft, my - 3 - b.ctx.canvas.offsetTop, 6, 6);
+
+    const possible = b.getPossiblePaths(yinsh.vertical, yinsh.point);
+    for (let index of possible) {
+      const vertical = (index / 11) | 0;
+      const point = index % 11;
+      const coord = b.getCanvasCoordinate(vertical, point);
+      b.ctx.fillRect(coord.x - 6, coord.y - 6, 12, 12);
+    }
 
     requestAnimationFrame(update);
   }
