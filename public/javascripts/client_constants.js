f7ac822a5cbe544de76f7151c0b83a7b20ec37f5
Vincent Groen <33351005+The-Coding-Sloth@users.noreply.github.com>
2020-01-02 16:28:12 +0100

    client board interaction

--- public/javascripts/client_constants.js
+++ public/javascripts/client_constants.js
@@ -3,4 +3,11 @@ const BLACK = 1;
 
 const INTERSECTIONS = [4, 7, 8, 9, 10, 9, 10, 9, 8, 7, 4];
 
-export { WHITE, BLACK, INTERSECTIONS };
+const SOCKET_URL = "ws://localhost:3000";
+const ROLES = { "waiting": 0, "playing": 1, "spectating": 2 };
+const TURN_TYPE = { "ring": 0, "marker": 1, "none": 2 };
+
+const AVAILABILITY = { "open": 0, "private": 1, "full": 2 };
+const ICONS = ["&#x1f513;", "&#x1f510;", "&#x1f441"];
+
+export { WHITE, BLACK, INTERSECTIONS, SOCKET_URL, ROLES, TURN_TYPE, AVAILABILITY, ICONS };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,6 @@
 import Socket from "./socket.js"
 import ClientBoard from "./client_board.js";
-
-const SOCKET_URL = "ws://localhost:3000";
-const ROLES = { "waiting": 0, "playing": 1, "spectating": 2 };
-const TURN_TYPE = { "ring": 0, "marker": 1, "none": 2 };
-let mx = 0, my = 0;
+import { SOCKET_URL, ROLES, TURN_TYPE } from "./client_constants.js";
 
 onload = () => {
   const socket = new Socket(new WebSocket(SOCKET_URL));
@@ -12,22 +8,34 @@ onload = () => {
   const url = new URL(window.location);
   const gameId = url.searchParams.get("id");
 
+  const mouse = { x: 0, y: 0 };
+  let animationFrame = null;
+  let pathsPerRing = null;
+  let targetRing = null;
+
+  let side = 2;
   let role = ROLES["waiting"];
-  let turnType = TURN_TYPE["none"];
+  let turnType = TURN_TYPE["marker"];
 
   socket.setReceive("join", data => {
     role = ROLES[data.role];
   });
 
+  socket.setReceive("side", data => {
+    side = data.side;
+  });
+
   socket.setReceive("turn", data => {
     if (data.turnNumber < 10) {
       turnType = TURN_TYPE["ring"];
-      // socket.send("turn", {
-      //   game: gameId,
-      //   from: { vertical: 0, point: 0 },
-      // });
+      cancelAnimationFrame(animationFrame);
+      animationFrame = requestAnimationFrame(update);
     } else {
       turnType = TURN_TYPE["marker"];
+      targetRing = null;
+      pathsPerRing = data.rings;
+      cancelAnimationFrame(animationFrame);
+      animationFrame = requestAnimationFrame(update);
       // socket.send("turn", {
       //   game: gameId,
       //   from: { vertical: 0, point: 0 },
@@ -39,7 +47,6 @@ onload = () => {
   socket.setReceive("boardUpdate", data => {
     board.rings = data.rings;
     board.markers = data.markers;
-    board.render();
   });
 
   socket.ws.onopen = () => {
@@ -50,9 +57,57 @@ onload = () => {
     };
     socket.send("join", properties);
   };
-}
 
-onmousemove = e => {
-  mx = e.pageX;
-  my = e.pageY;
+  onmousemove = e => {
+    mouse.x = e.pageX - board.ctx.canvas.offsetLeft;
+    mouse.y = e.pageY - board.ctx.canvas.offsetTop;
+  }
+
+  onclick = e => {
+    mouse.x = e.pageX - board.ctx.canvas.offsetLeft;
+    mouse.y = e.pageY - board.ctx.canvas.offsetTop;
+
+    if (e.button == 0) {
+
+      if (turnType == TURN_TYPE["ring"]) {
+        board.validateRing(mouse.x, mouse.y, (vertical, point) => {
+          socket.send("turn", { game: gameId, from: { vertical, point } });
+          turnType = TURN_TYPE["none"];
+          cancelAnimationFrame(animationFrame);
+        });
+      }
+
+      if (turnType == TURN_TYPE["marker"]) {
+        board.validateMarker(mouse.x, mouse.y, side, (vertical, point) => targetRing = { vertical, point });
+
+        if (targetRing != null) {
+          board.validateRing(mouse.x, mouse.y, (vertical, point) => {
+            const index = vertical * 11 + point;
+
+            if (pathsPerRing[targetRing.vertical * 11 + targetRing.point].includes(index)) {
+              socket.send("turn", {
+                game: gameId,
+                from: targetRing,
+                to: { vertical, point }
+              });
+            }
+          });
+        }
+      }
+    }
+  }
+
+  function update() {
+    board.render();
+
+    if (turnType == TURN_TYPE["ring"])
+      board.validateRing(mouse.x, mouse.y, (vertical, point) => board.drawRing(vertical, point, side, true));
+    else if (turnType == TURN_TYPE["marker"]) {
+      board.validateMarker(mouse.x, mouse.y, side, (vertical, point) => board.drawMarker(vertical, point, side, true));
+
+      if (targetRing != null) board.drawMarker(targetRing.vertical, targetRing.point, side, true);
+    }
+
+    animationFrame = requestAnimationFrame(update);
+  };
 }
